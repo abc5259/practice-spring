bf5892926f0ad7fa5f431080959b97895e5ec352
LeeJaeHoon <dlwogns3413@naver.com>
2022-05-27 04:28:31 +0900

    test: MemberRepository Test clear

--- src/main/java/hello/javaspring/controller/HelloController.java
+++ src/main/java/hello/javaspring/controller/HelloController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller()
 public class HelloController {
     //Static
+    // /hello로 가면 이 controller가 실행된다.
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-        return  "hello"; // templates/hello.html 을 찾아간다
+        return  "hello"; // resources/templates/hello.html 을 찾아간다 viewResolver가 찾아줌!
     }
 
     //MVC
@@ -24,9 +25,9 @@ public class HelloController {
 
     //API
     @GetMapping("hello-string")
-    @ResponseBody
+    @ResponseBody // response body에 넣어준다는 뜻
     public String helloString(@RequestParam("name") String name) {
-        return  "hello" + name;
+        return  "hello " + name;
     }
 
     @GetMapping("hello-api")
@@ -37,15 +38,14 @@ public class HelloController {
         return hello; //객체를 return 하면 json 형태로 반환한다.
     }
 
-    static class Hello {
+    static class Hello { //static으로 하면 class안에서 class사용 가능
         private String name;
-
         public String getName() {
             return  name;
         }
-
         public void setName(String name) {
             this.name = name;
         }
+
     }
 }
