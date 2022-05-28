e28f172c476350ff2ecbe3122e38829a29a59aea
LeeJaeHoon <dlwogns3413@naver.com>
2022-05-29 01:36:01 +0900

    feat: db connect and 통합 test clear

--- src/main/java/hello/javaspring/controller/MemberController.java
+++ src/main/java/hello/javaspring/controller/MemberController.java
@@ -1,16 +1,45 @@
 package hello.javaspring.controller;
 
+
+import hello.javaspring.domain.Member;
 import hello.javaspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.model.IModelVisitor;
+
+import java.util.List;
 
 @Controller //스프링 컨테이너에 MemberController 객체를 생성해서 넣어둔다
 public class MemberController {
 
-    private final MemberService memberService;
+    private MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
